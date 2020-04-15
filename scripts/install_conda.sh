b63a78c4220bd0b581118750b0d64dd27262a32c
moto <855818+mthrok@users.noreply.github.com>
2020-04-15 08:40:12 -0700

    Fix conda install

--- scripts/install_conda.sh
+++ scripts/install_conda.sh
@@ -5,9 +5,10 @@ set -euxo pipefail
 prefix="$1"
 installer="Miniconda3-latest-Linux-x86_64.sh"
 
-curl -sO https://repo.continuum.io/miniconda/"${installer}"
+curl -sLO http://repo.continuum.io/miniconda/"${installer}"
 bash -x "${installer}" -b -p "${prefix}"
 rm "${installer}"
 
 eval "$(${prefix}/bin/conda shell.bash hook)"
 conda init
+conda update -n base -c defaults conda
